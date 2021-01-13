35e46be92f464fe23241739d04b4d502f1f5e75d
James Glover <james.e.glover1@gmail.com>
2021-01-12 18:05:46 -0600

    started side bar

---
+++ FED1StudioProject/scripts/main.js
@@ -0,0 +1,11 @@
+﻿/*side nav functions*/
+
+function openAdminNav() {
+    document.getElementById('admin-btn').style.display = 'none';
+    document.getElementById('admin-nav').style.width = '250px';
+}
+
+function closeAdminNav() {
+    document.getElementById('admin-btn').style.display = 'block';
+    document.getElementById('admin-nav').style.width = '0';
+}
